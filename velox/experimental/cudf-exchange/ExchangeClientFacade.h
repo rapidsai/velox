21483c9aee448a8a78e7ff9112c1674c645efee9
Daniel Bauer <dnb@zurich.ibm.com>
2026-01-16 13:25:21 +0000

    Migrated cudf-exchange to OSS velox version from 2026-01-09.

--- velox/experimental/cudf-exchange/ExchangeClientFacade.h
+++ velox/experimental/cudf-exchange/ExchangeClientFacade.h
@@ -25,7 +25,7 @@ namespace facebook::velox::cudf_exchange {
 
 // Define the return types from the two types of exchange clients as a variant
 using SerPageVector =
-    std::vector<std::unique_ptr<facebook::velox::exec::SerializedPage>>;
+    std::vector<std::unique_ptr<facebook::velox::exec::SerializedPageBase>>;
 using PackedColPtr = std::unique_ptr<cudf::packed_columns>;
 using ResultVariant = std::variant<SerPageVector, PackedColPtr>;
 
