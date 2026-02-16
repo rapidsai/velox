d0127fb78f0f77533d98bb20b9c695d89eddd194
Daniel Bauer <dnb@zurich.ibm.com>
2026-02-16 09:48:13 +0000

    Wire CudfOutputQueueManager into Task for broadcast support
    Pass the output node's kind (partitioned/broadcast) to
    CudfOutputQueueManager::initializeTask() and forward
    Task::updateOutputBuffers() calls to the cudf queue manager
    so that broadcast destination count updates reach the cudf
    exchange layer.

--- velox/exec/Task.cpp
+++ velox/exec/Task.cpp
@@ -1188,6 +1188,7 @@ void Task::initializePartitionOutput() {
           getInstanceRef();
       queueMgr->initializeTask(
           shared_from_this(),
+          partitionedOutputNode->kind(),
           partitionedOutputNode->numPartitions(),
           numOutputDrivers);
     }
@@ -2182,7 +2183,18 @@ bool Task::updateOutputBuffers(int numBuffers, bool noMoreBuffers) {
       noMoreOutputBuffers_ = true;
     }
   }
-  return bufferManager->updateOutputBuffers(taskId_, numBuffers, noMoreBuffers);
+  auto result =
+      bufferManager->updateOutputBuffers(taskId_, numBuffers, noMoreBuffers);
+#ifdef VELOX_ENABLE_CUDF
+  if (velox::cudf_velox::CudfConfig::getInstance().enabled &&
+      velox::cudf_velox::CudfConfig::getInstance().exchange) {
+    auto queueMgr =
+        facebook::velox::cudf_exchange::CudfOutputQueueManager::
+            getInstanceRef();
+    queueMgr->updateOutputBuffers(taskId_, numBuffers, noMoreBuffers);
+  }
+#endif
+  return result;
 }
 
 int Task::getOutputPipelineId() const {
