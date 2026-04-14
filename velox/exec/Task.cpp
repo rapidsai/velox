1803c118a16623fa5a7ef71ce0bb8f9df0e00dce
Karthikeyan Natarajan <karthikeyann@users.noreply.github.com>
2026-04-14 03:45:12 +0000

    fix failing test

--- velox/exec/Task.cpp
+++ velox/exec/Task.cpp
@@ -3078,7 +3078,9 @@ folly::dynamic Task::toJson() const {
       }
     }
   }
-  obj["bufferManagers"] = bufferManagers;
+  if (!bufferManagers.empty()) {
+    obj["bufferManagers"] = bufferManagers;
+  }
 
   folly::dynamic exchangeClients = folly::dynamic::object;
   for (const auto& [id, client] : exchangeClientByPlanNode_) {
